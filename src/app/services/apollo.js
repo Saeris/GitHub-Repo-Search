00759f38792bca1ff31227f8a27d38515a8cbb75
Drake Costa <Saeris@comcast.net>
2018-07-02 13:33:10 -0700

    Fix Miss-match with REACT_APP_GITHUB_TOKEN
    src/app/services/apollo.js was looking for the wrong environment variable

--- src/app/services/apollo.js
+++ src/app/services/apollo.js
@@ -4,8 +4,9 @@ export const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   request: async operation => {
     const token =
-      (await localStorage.getItem("token")) || process.env.REACT_APP_TOKEN
-    console.log(process.env.REACT_APP_TOKEN)
+      (await localStorage.getItem("token")) ||
+      process.env.REACT_APP_GITHUB_TOKEN
+    //console.log(process.env.REACT_APP_GITHUB_TOKEN)
     operation.setContext({
       headers: {
         authorization: `Bearer ${token}`
